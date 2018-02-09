cf809045b291f50241c4742b1b075583e1ee4026
lihongwei <lihongwei@lihongweis-MacBook-Pro.local>
2018-02-09 14:45:16 +0800

    add bitfinex

--- exchange/okex.py
+++ exchange/okex.py
@@ -94,27 +94,31 @@ class okexUtil:
 	async def order_book(self,trade_handler):
 		channel='ok_sub_spot_'+self.CURRENT_PAIR+'_depth_5'
 		while True:
-			async with websockets.connect('wss://real.okex.com:10441/websocket') as websocket:
-				try:	
-					param={'event':'addChannel','channel':channel}
-					await websocket.send(json.dumps(param))
-					while True:
-						message = await websocket.recv()
-						res=json.loads(message)
-						if type(res) is list and res[0]['channel'].startswith('ok'):
-							ask_map={}
-							for item in res[0]['data']['asks']:
-								ask_map[item[0]]=float(item[1])
-							self.ORDER_BOOK['ask']=ask_map
-							bid_map={}
-							for item in res[0]['data']['bids']:
-								bid_map[item[0]]=float(item[1])
-							self.ORDER_BOOK['bid']=bid_map
-						await trade_handler()
-				except  Exception as e:
-					self.ORDER_BOOK={}
-					logger.error(e)
-					websocket.close()
+			try:
+				async with websockets.connect('wss://real.okex.com:10441/websocket') as websocket:
+					try:
+						param={'event':'addChannel','channel':channel}
+						await websocket.send(json.dumps(param))
+						while True:
+							message = await websocket.recv()
+							res=json.loads(message)
+							if type(res) is list and res[0]['channel'].startswith('ok'):
+								ask_map={}
+								for item in res[0]['data']['asks']:
+									ask_map[item[0]]=float(item[1])
+								self.ORDER_BOOK['ask']=ask_map
+								bid_map={}
+								for item in res[0]['data']['bids']:
+									bid_map[item[0]]=float(item[1])
+								self.ORDER_BOOK['bid']=bid_map
+							await trade_handler()
+					except  Exception as e:
+						websocket.close()
+						logger.error('OKEX BOOK parse:{}'.format(e))
+			except  Exception as le:
+				self.ORDER_BOOK={}
+				logger.error('OKEX BOOK connect:{}'.format(e))
+
 	def get_orderbook_head(self):
 		if len(self.ORDER_BOOK)>0:
 			ask_head=min(self.ORDER_BOOK['ask'],key=lambda subItem:float(subItem))
