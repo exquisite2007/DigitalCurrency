f95670f324ae6107c8f2ff8da04776f7504fad75
lihongwei <lihongwei@lihongweis-MacBook-Pro.local>
2018-03-30 11:09:08 +0800

    change base amount

--- grid.py
+++ grid.py
@@ -33,7 +33,7 @@ INSERT_SYSTEM_SQL='insert into system (key,value) values(?,?)'
 conn = sqlite3.connect('trade.db')
 LAST_TRADE_PRICE_KEY='GRID_LAST_TRADE_PRICE'
 LAST_TRADE_PRICE=None
-BASE_TRADE_AMOUNT=1
+BASE_TRADE_AMOUNT=3
 TRADE_LOCK=False
 STATE='W'
 
@@ -94,7 +94,8 @@ async def trade():
 		if diff_rate >SELL_RATE_THRESHOLD: #上段，数字币远多于法币
 			STATE='DG'
 			if ORDER_ID is None:#
-				pass#TODO:处理上涨太快
+				LAST_TRADE_PRICE=last
+				logger.error('price grow too fast,force chande last price:{}'.format('LAST_TRADE_PRICE'))
 			else:#从中上段 进入上段
 				order_res = await util.order_info(ORDER_ID)
 				if len(order_res)>0 and order_res[0]['status']==2:
@@ -139,7 +140,8 @@ async def trade():
 		elif  -diff_rate > BUY_RATE_THRESHOLD:#下段，法币远多于数字币，不平衡状态
 			STATE='DG'
 			if ORDER_ID is None:#
-				pass#TODO:处理下跌太快
+				LAST_TRADE_PRICE=last
+				logger.error('price drop too fast,force chande last price:{}'.format('LAST_TRADE_PRICE'))
 			else:#从中下段 进入下段
 				order_res = await util.order_info(ORDER_ID)
 				if len(order_res)>0 and order_res[0]['status']==2:
